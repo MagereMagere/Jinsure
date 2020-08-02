fdfc29c7a1806fd0b612a1d6c873b7e0e3f416cd
Magere <jackboloo101@gmail.com>
2020-08-02 20:18:16 +0300

    requirements.txt

--- base_main/urls.py
+++ base_main/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
 	# admin
     path('c0d3b453/', admin.site.urls),
@@ -25,3 +28,7 @@ urlpatterns = [
     path('self-assessment/', include('portal_risk.urls')),
     path('file-a-claim/', include('claim.urls')),
 ]
+# serve the uploaded media files using the development server
+if settings.DEBUG:
+    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
